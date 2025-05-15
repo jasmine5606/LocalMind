8971bd3ad1e23aa5b41a1e0021d4b813065e4bed
Chen Jinhua <2538319991@qq.com>
2025-05-15 16:49:00 +0800

    feat: implement voucher management with normal and seckill types

---
+++ src/main/java/com/huixiang/service/IVoucherService.java
@@ -0,0 +1,20 @@
+package com.huixiang.service;
+
+import com.huixiang.dto.Result;
+import com.huixiang.entity.Voucher;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author 虎哥
+ * @since 2021-12-22
+ */
+public interface IVoucherService extends IService<Voucher> {
+
+    Result queryVoucherOfShop(Long shopId);
+
+    void addSeckillVoucher(Voucher voucher);
+}
